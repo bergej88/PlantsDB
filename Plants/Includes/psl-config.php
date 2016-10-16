377c210fb906aab2ea44f774490073711af08535
Ben <ben.kletzine@gmail.com>
2016-10-16 15:55:10 -0500

    Working on adding a plant

--- Plants/Includes/psl-config.php
+++ Plants/Includes/psl-config.php
@@ -2,10 +2,10 @@
 /**
  * These are the database login details
  */
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "sec_user");    // The database username.
-define("PASSWORD", "eKcGZr59zAa2BEWU");    // The database password.
-define("DATABASE", "secure_login");    // The database name.
+define("HOST", "webdev.cs.uwosh.edu");     // The host you want to connect to.
+define("USER", "bergej88");    // The database username.
+define("PASSWORD", "plantsDatbase");    // The database password.
+define("DATABASE", "bergej88");    // The database name.
 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
