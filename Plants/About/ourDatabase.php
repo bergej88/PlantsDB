914a074be9724599ee37cad44b30549882ea7068
sprinm40 <sprinm40@uwosh.edu>
2016-10-23 16:50:06 -0500

    Put in some dummy data for ourDatabase page.

--- Plants/About/ourDatabase.php
+++ Plants/About/ourDatabase.php
@@ -1,7 +1,53 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sprinm40
- * Date: 10/23/2016
- * Time: 3:22 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="UWO Project - Remake of USDA Plant Database">
+    <meta name="keywords" content="">
+    <meta name="author" content="Matt Springer, Ben Kletzine, Jeff Berger">
+
+    <title>Layout Template</title>
+
+    <!-- Styles -->
+    <link href="../Content/bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet">
+    <link href="../Content/styles/main.css" rel="stylesheet">
+
+</head>
+<body>
+<?php include('../Layouts/topNav.php') ?>
+
+
+<!-- ============================== -->
+<!-- == Content Section          == -->
+<!-- ============================== -->
+
+<?php include('../Layouts/contentStart.php')?>
+<p>
+    The PLANTS Database
+
+    The PLANTS Database provides standardized information about the vascular plants, mosses, liverworts, hornworts, and lichens of the U.S. and its territories. It includes names, plant symbols, checklists, distributional data, species abstracts, characteristics, images, crop information, automated tools, onward Web links, and references. This information primarily promotes land conservation in the United States and its territories, but academic, educational, and general use is encouraged. PLANTS reduces government spending by minimizing duplication and making information exchange possible across agencies and disciplines.
+
+    PLANTS is a collaborative effort of the USDA NRCS National Plant Data Team (NPDT), the USDA NRCS Information Technology Center (ITC), The USDA National Information Technology Center (NITC), and many other partners. Much of the PLANTS data and design is developed at NPDT, and the Web application is programmed at ITC and NITC and served through the USDA Web Farm. Here’s more information about who does what on the PLANTS Team, our Partners, and our Data Contributors.
+
+    How to cite the PLANTS Database.
+    Learn more about the data at PLANTS.
+    See additional PLANTS Documentation.
+    Other questions are answered at the PLANTS FAQs.
+    Need more information? Please check our PLANTS Help topics.
+
+<?php include('../Layouts/contentEnd.php')?>
+
+
+
+<!-- ============================== -->
+<!-- == Script Section           == -->
+<!-- ============================== -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="../Content/jQuery/jquery-3.1.1.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../Content/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+</body>
+</html>
